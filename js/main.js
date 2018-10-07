d2cec0d684a86f00f182f94b6d3eecb2df3c9ed2
unknown <alex420.wz@gmail.com>
2018-10-06 21:15:44 -0500

    primer commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const ponerColor = () =>
+{
+  document.getElementById('perfil').style.background = "#097899";
+}
+const regresarColor = () =>
+{
+  document.getElementById('perfil').style.background = "#00aadc";
+}
+const ponerColor1 = () =>
+{
+  document.getElementById('boton1').style.background = "#d5d8da";
+}
+const regresarColor2 = () =>
+{
+  document.getElementById("boton1").style.background = "#e9eff3";
+}
+const ponerColor3 = () =>
+{
+  document.getElementById('boton2').style.background = "#d5d8da";
+}
+const regresarColor4 = () =>
+{
+  document.getElementById("boton2").style.background = "#e9eff3";
+}
+const ponerColor5 = () =>
+{
+  document.getElementById('boton3').style.background = "#d5d8da";
+}
+const regresarColor6 = () =>
+{
+  document.getElementById("boton3").style.background = "#e9eff3";
+}
+const mandarAlerta = () =>
+{
+  alert("Tus datos fueron registrados con éxito =)")
+  document.location.href='perfil.html';
+}
